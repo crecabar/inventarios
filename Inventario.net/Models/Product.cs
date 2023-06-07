842706f2fbda2f41cf94ccb43f10da9982345d5f
Cristian Recabarren <cristian.recabarren@rglabs.cl>
2023-06-06 20:27:08 -0400

    Added Product model class

---
+++ Inventario.net/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Inventario.net.Models
+{
+	public class Product
+	{
+		public int ID { get; set; }
+		public string? Name { get; set; }
+		public int CurrentAmount { get; set; }
+		public int CountedAmount { get; set; }
+	}
+}
+
